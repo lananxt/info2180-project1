f2c81138117e705c220106af50e5f6e8d4e9e86e
Alana Thompson <alana_thompson@yahoo.com>
2021-10-24 18:54:24 -0500

    Made final changes to the styles sheet and scripts

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
 /* Add your JavaScript to this file */
+document.getElementsByClassName("btn").addEventListener("click", function() {
+  document.getElementsByClassName("message").innerHTML = "Thank you! Your email address has been added to our mailing list";
+});
